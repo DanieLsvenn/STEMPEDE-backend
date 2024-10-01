18a8ed478ecdacbb4ee4aa56c14a46be6b92f32e
hoangmt202 <jeruchi888@gmail.com>
2024-10-01 20:23:48 +0700

    add BE generic repository,dbContext, unitofWork, business, asp.web api,initialize project with 3 layers architecture

---
+++ SWP391__StempedeKit_FA24/SWP391__StempedeKit_FA24/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace SWP391__StempedeKit_FA24
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
