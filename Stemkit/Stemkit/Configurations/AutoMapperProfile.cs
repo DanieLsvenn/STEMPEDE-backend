1629c4a1a6096f4f779ea575bad686a9b74d8f4f
Sadlight <lucthanhthuy29@gmail.com>
2024-10-24 06:08:12 +0700

    Rework regis and login to use auto mapper. Add configuration settings for database

--- Stemkit/Stemkit/Configurations/AutoMapperProfile.cs
+++ Stemkit/Stemkit/Configurations/AutoMapperProfile.cs
@@ -2,6 +2,7 @@
 using Stemkit.DTOs.Product;
 using Stemkit.Models;
 using AutoMapper;
+using Stemkit.DTOs.Auth;
 
 namespace Stemkit.Configurations
 {
@@ -9,12 +10,20 @@ namespace Stemkit.Configurations
     {
         public AutoMapperProfile()
         {
+            //Product mappings
             CreateMap<Product, ReadProductDto>()
                 .ForMember(dest => dest.LabName, opt => opt.MapFrom(src => src.Lab.LabName))
                 .ForMember(dest => dest.SubcategoryName, opt => opt.MapFrom(src => src.Subcategory.SubcategoryName));
 
             CreateMap<CreateProductDto, Product>();
             CreateMap<UpdateProductDto, Product>();
+
+            // MappingsAuthentication
+            CreateMap<UserRegistrationDto, User>()
+                .ForMember(dest => dest.Password, opt => opt.Ignore())
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => true)) // Default status
+                .ForMember(dest => dest.IsExternal, opt => opt.MapFrom(src => src.IsExternal))
+                .ForMember(dest => dest.ExternalProvider, opt => opt.MapFrom(src => src.ExternalProvider ?? null));
         }
     }
 }
