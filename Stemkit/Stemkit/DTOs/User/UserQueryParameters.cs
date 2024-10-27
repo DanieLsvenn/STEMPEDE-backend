b90b47392351e74512da08488e07fff4254bb73a
Sadlight <lucthanhthuy29@gmail.com>
2024-10-28 00:35:01 +0700

    Add get all user pagination for admin controller. Rework all method in admin controller for better return

---
+++ Stemkit/Stemkit/DTOs/User/UserQueryParameters.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Stemkit.DTOs.User
+{
+    public class UserQueryParameters
+    {
+        private const int maxPageSize = 100;
+        private int _pageSize = 10;
+
+        /// <summary>
+        /// The page number to retrieve.
+        /// </summary>
+        [Range(1, int.MaxValue, ErrorMessage = "Page number must be at least 1.")]
+        public int PageNumber { get; set; } = 1;
+
+        /// <summary>
+        /// The number of items per page.
+        /// </summary>
+        [Range(1, maxPageSize, ErrorMessage = "Page size must be between 1 and 100.")]
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > maxPageSize) ? maxPageSize : value;
+        }
+    }
+}
