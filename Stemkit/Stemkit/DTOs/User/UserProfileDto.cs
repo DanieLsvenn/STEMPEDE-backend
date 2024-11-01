6ab06df11910d0c66a4653ac00f95a847567d831
Sadlight <lucthanhthuy29@gmail.com>
2024-11-01 07:35:31 +0100

    Add missing file

---
+++ Stemkit/Stemkit/DTOs/User/UserProfileDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Stemkit.DTOs.User
+{
+    public class UserProfileDto
+    {
+        public int UserID { get; set; }
+        public string FullName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public bool Status { get; set; }
+    }
+}
